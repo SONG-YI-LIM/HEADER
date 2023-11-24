4c5b4de3817d2aebe278dab0ccb4ff594c19e66f
SONG-YI-LIM <iamlsy.da@gmail.com>
2023-11-24 15:42:15 +0900

    유니쿼스 완료

--- 03. 유니쿼스/assets/js/common.js
+++ 03. 유니쿼스/assets/js/common.js
@@ -4,20 +4,74 @@ function fn_layout() {
 	const FOOTER = $('#footer');
 	const GNB = $('#gnb');
 	const GNBWRAP = $('.gnbWrap');
+	const BTNAll = $('.btnAllMenu');
 
 	// CONTAINER HEIGHT
 	// $(window).resize(function(){
 	// 	CONTAINER.css({'min-height':$(window).height() - HEADER.outerHeight() - FOOTER.outerHeight()});
 	// }).resize();
 
-	GNB.mouseover(function(){
+	GNB.mouseenter(function(){
 		HEADER.addClass('open');
-		
-		GNBWRAP.mouseout(function(){
-			HEADER.removeClass('open');
-			console.log("떠나라")
-		});
 	});
+
+	GNBWRAP.mouseleave(function(){
+		HEADER.removeClass('open');
+	});
+
+	BTNAll.on('click', function(){
+		if($(this).hasClass('close')){
+			$(this).removeClass('close').text('전체메뉴 보기');
+			HEADER.removeClass('open');
+			GNBWRAP.mouseleave(function(){
+				HEADER.removeClass('open');
+			});
+		}else{
+			$(this).addClass('close').text('전체메뉴 닫기');
+			HEADER.addClass('open');
+			GNBWRAP.off();
+		}
+	});
+
+	
+	GNB.find('> li').each(function(){
+		let isMore = $(this).has('ul').length;
+		if(isMore) $(this).addClass('more');
+	});
+
+	GNB.on('focusin', '> li > a', function(){
+		$(this).parent().addClass('focus');
+		HEADER.addClass('open');
+		if($(this).parent().hasClass('more')){
+			GNB.on('focusout', 'ul li:last-child > a', function(){
+				$(this).parents('li').removeClass('focus');
+			});
+		}else{
+			$(this).on('focusout', function(){
+				$(this).parent().removeClass('focus');
+			});
+		}
+	});
+
+	GNB.on('focusout', '> li:last-child > a', function(){
+		$(this).parent().removeClass('focus');
+		HEADER.removeClass('open');
+	});
+
+	// GNB.on('focusin', '> li > a', function(){
+	// 	$(this).parent().addClass('focus');
+	// 	if($(this).parent().hasClass('more')){
+	// 		GNB.on('focusout', 'ul li:last-child > a', function(){
+	// 			$(this).parents('li').removeClass('focus');
+	// 		});
+	// 	}else{
+	// 		$(this).on('focusout', function(){
+	// 			$(this).parent().removeClass('focus');
+	// 		});
+	// 	}
+	// });
+
+
 }
 
 $(function() {
