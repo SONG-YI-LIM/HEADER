4135757dcef2b26ea003be9cd0c61fde7fba7fbf
SONG-YI-LIM <iamlsy.da@gmail.com>
2023-12-01 15:55:23 +0900

    유니쿼스 완료

--- 03. 유니쿼스/assets/js/common.js
+++ 03. 유니쿼스/assets/js/common.js
@@ -5,19 +5,39 @@ function fn_layout() {
 	const GNB = $('#gnb');
 	const GNBWRAP = $('.gnbWrap');
 	const BTNAll = $('.btnAllMenu');
+	const BTNMENU = $('.btnMenu');
 
-	// CONTAINER HEIGHT
-	// $(window).resize(function(){
-	// 	CONTAINER.css({'min-height':$(window).height() - HEADER.outerHeight() - FOOTER.outerHeight()});
-	// }).resize();
-
-	GNB.mouseenter(function(){
-		HEADER.addClass('open');
+	// 전체메뉴
+	BTNAll.on('click', function(){
+		if($(this).hasClass('close')){
+			$(this).removeClass('close').text('전체메뉴 보기');
+			HEADER.removeClass('open');
+			GNBWRAP.mouseleave(function(){
+				HEADER.removeClass('open');
+			});
+		}else{
+			$(this).addClass('close').text('전체메뉴 닫기');
+			HEADER.addClass('open');
+			GNBWRAP.off();
+		}
 	});
 
-	GNBWRAP.mouseleave(function(){
-		HEADER.removeClass('open');
+	// GNB 2DEPTH
+	GNB.find('> li').each(function(){
+		let isMore = $(this).has('ul').length;
+		if(isMore) $(this).addClass('more');
 	});
+
+	// GNB
+	function mouseEffect(){
+		GNB.mouseenter(function(){
+			HEADER.addClass('open');
+		});
+		GNBWRAP.mouseleave(function(){
+			HEADER.removeClass('open');
+		});
+	}
+	mouseEffect();
 
 	$(window).resize(function(){
 		if($(window).width() < 1161){
@@ -33,7 +53,13 @@ function fn_layout() {
 					$(this).next().slideDown(200);
 				}
 			});
+
+			BTNMENU.on('click', function(){
+				HEADER.toggleClass('open');
+
+			});
 		}else{
+			mouseEffect();
 			GNB.on('focusin', '> li > a', function(){
 				$(this).parent().addClass('focus');
 				HEADER.addClass('open');
@@ -54,43 +80,6 @@ function fn_layout() {
 			});
 		}
 	}).resize();
-
-	BTNAll.on('click', function(){
-		if($(this).hasClass('close')){
-			$(this).removeClass('close').text('전체메뉴 보기');
-			HEADER.removeClass('open');
-			GNBWRAP.mouseleave(function(){
-				HEADER.removeClass('open');
-			});
-		}else{
-			$(this).addClass('close').text('전체메뉴 닫기');
-			HEADER.addClass('open');
-			GNBWRAP.off();
-		}
-	});
-
-	
-	GNB.find('> li').each(function(){
-		let isMore = $(this).has('ul').length;
-		if(isMore) $(this).addClass('more');
-	});
-
-	
-
-	// GNB.on('focusin', '> li > a', function(){
-	// 	$(this).parent().addClass('focus');
-	// 	if($(this).parent().hasClass('more')){
-	// 		GNB.on('focusout', 'ul li:last-child > a', function(){
-	// 			$(this).parents('li').removeClass('focus');
-	// 		});
-	// 	}else{
-	// 		$(this).on('focusout', function(){
-	// 			$(this).parent().removeClass('focus');
-	// 		});
-	// 	}
-	// });
-
-
 }
 
 $(function() {
