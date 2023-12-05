799661a222b96f62fd673c4a34ab666b87a6c890
SONG-YI-LIM <iamlsy.da@gmail.com>
2023-12-05 10:43:48 +0900

    2023-12-05

--- 04. 소방안전빅데이터플랫폼/assets/js/common.js
+++ 04. 소방안전빅데이터플랫폼/assets/js/common.js
@@ -2,83 +2,10 @@ function fn_layout() {
 	const HEADER = $('#header');
 	const CONTAINER = $('#container');
 	const FOOTER = $('#footer');
-	const GNB = $('#gnb');
-	const GNBWRAP = $('.gnbWrap');
-	const BTNAll = $('.btnAllMenu');
-	const BTNMENU = $('.btnMenu');
 
-	// 전체메뉴
-	BTNAll.on('click', function(){
-		if($(this).hasClass('close')){
-			$(this).removeClass('close').text('전체메뉴 보기');
-			HEADER.removeClass('open');
-			GNBWRAP.mouseleave(function(){
-				HEADER.removeClass('open');
-			});
-		}else{
-			$(this).addClass('close').text('전체메뉴 닫기');
-			HEADER.addClass('open');
-			GNBWRAP.off();
-		}
-	});
-
-	// GNB 2DEPTH
-	GNB.find('> li').each(function(){
-		let isMore = $(this).has('ul').length;
-		if(isMore) $(this).addClass('more');
-	});
-
-	// GNB
-	function mouseEffect(){
-		GNB.mouseenter(function(){
-			HEADER.addClass('open');
-		});
-		GNBWRAP.mouseleave(function(){
-			HEADER.removeClass('open');
-		});
-	}
-	mouseEffect();
-
+	// CONTAINER HEIGHT
 	$(window).resize(function(){
-		if($(window).width() < 1161){
-			GNB.off();
-			GNBWRAP.off();
-			GNB.on('click', '> li > a', function(e){
-				e.preventDefault();
-				if($(this).hasClass('open')){
-					$(this).removeClass('open');
-					$(this).next().slideUp(200);
-				}else{
-					$(this).addClass('open');
-					$(this).next().slideDown(200);
-				}
-			});
-
-			BTNMENU.on('click', function(){
-				HEADER.toggleClass('open');
-
-			});
-		}else{
-			mouseEffect();
-			GNB.on('focusin', '> li > a', function(){
-				$(this).parent().addClass('focus');
-				HEADER.addClass('open');
-				if($(this).parent().hasClass('more')){
-					GNB.on('focusout', 'ul li:last-child > a', function(){
-						$(this).parents('li').removeClass('focus');
-					});
-				}else{
-					$(this).on('focusout', function(){
-						$(this).parent().removeClass('focus');
-					});
-				}
-			});
-		
-			GNB.on('focusout', '> li:last-child > a', function(){
-				$(this).parent().removeClass('focus');
-				HEADER.removeClass('open');
-			});
-		}
+		CONTAINER.css({'min-height':$(window).height() - HEADER.outerHeight() - FOOTER.outerHeight()});
 	}).resize();
 }
 
